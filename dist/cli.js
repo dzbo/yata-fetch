6c1cb5cb5778499910aa065f621bb2ebf50a7dec
Dominik Zborowski <dominik.zborowski@gmail.com>
2020-03-24 13:48:36 +0100

    Regenerate dist files

--- dist/cli.js
+++ dist/cli.js
@@ -1,36 +1,110 @@
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+"use strict";
 
-const yata = require('./yata');
-const nconf = require('nconf');
-const log = require('./log');
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-module.exports = _asyncToGenerator(function* () {
-  // read argv for potential custom config path
-  nconf.argv();
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
-  // read ENV for token
-  nconf.env();
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 
-  // load config path
-  nconf.file({ file: yata.getConfigPath(nconf.get('config')) });
+function _createForOfIteratorHelper(o) { if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (o = _unsupportedIterableToArray(o))) { var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var it, normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 
-  // setup API host
-  yata.apiHost = nconf.get('YATA_API_HOST') || 'https://api.yatapp.net/';
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
-  try {
-    if (yata.validateConfig(nconf.get(nconf.get('token')), nconf.get('project'), nconf.get('locales'), nconf.get('format'), nconf.get('root'), nconf.get('outputPath'), nconf.get('strip_empty'))) {
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-      // if passed locale explicit download just one
-      if (nconf.get('locale')) {
-        yield yata.downloadTranslation(nconf.get('locale'));
-      } else {
-        for (let locale of yata.locales) {
-          yield yata.downloadTranslation(locale);
-        }
+var yata = require('./yata');
+
+var nconf = require('nconf');
+
+var log = require('./log');
+
+module.exports = /*#__PURE__*/(0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee() {
+  var _iterator, _step, locale;
+
+  return _regenerator["default"].wrap(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          // read argv for potential custom config path
+          nconf.argv(); // read ENV for token
+
+          nconf.env(); // load config path
+
+          nconf.file({
+            file: yata.getConfigPath(nconf.get('config'))
+          }); // setup API host
+
+          yata.apiHost = nconf.get('YATA_API_HOST') || 'https://api.yatapp.net/';
+          _context.prev = 4;
+
+          if (!yata.validateConfig(nconf.get(nconf.get('token')), nconf.get('project'), nconf.get('locales'), nconf.get('format'), nconf.get('root'), nconf.get('outputPath'), nconf.get('strip_empty'))) {
+            _context.next = 28;
+            break;
+          }
+
+          if (!nconf.get('locale')) {
+            _context.next = 11;
+            break;
+          }
+
+          _context.next = 9;
+          return yata.downloadTranslation(nconf.get('locale'));
+
+        case 9:
+          _context.next = 28;
+          break;
+
+        case 11:
+          _iterator = _createForOfIteratorHelper(yata.locales);
+          _context.prev = 12;
+
+          _iterator.s();
+
+        case 14:
+          if ((_step = _iterator.n()).done) {
+            _context.next = 20;
+            break;
+          }
+
+          locale = _step.value;
+          _context.next = 18;
+          return yata.downloadTranslation(locale);
+
+        case 18:
+          _context.next = 14;
+          break;
+
+        case 20:
+          _context.next = 25;
+          break;
+
+        case 22:
+          _context.prev = 22;
+          _context.t0 = _context["catch"](12);
+
+          _iterator.e(_context.t0);
+
+        case 25:
+          _context.prev = 25;
+
+          _iterator.f();
+
+          return _context.finish(25);
+
+        case 28:
+          _context.next = 33;
+          break;
+
+        case 30:
+          _context.prev = 30;
+          _context.t1 = _context["catch"](4);
+          log('red', _context.t1);
+
+        case 33:
+        case "end":
+          return _context.stop();
       }
     }
-  } catch (e) {
-    log('red', e);
-  }
-});
+  }, _callee, null, [[4, 30], [12, 22, 25, 28]]);
+}));
 //# sourceMappingURL=cli.js.map
--- dist/yata.js
+++ dist/yata.js
@@ -1,7 +1,12 @@
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-const log = require('./log');
+"use strict";
+
+var https = require('https');
+
+var fs = require('fs');
+
+var path = require('path');
+
+var log = require('./log');
 
 module.exports = {
   config: null,
@@ -15,8 +20,7 @@ module.exports = {
   outputPath: 'translations',
   stripEmpty: false,
   apiHost: null,
-
-  getConfigPath(configPath) {
+  getConfigPath: function getConfigPath(configPath) {
     if (configPath) {
       this.configPath = configPath;
     } else {
@@ -25,8 +29,7 @@ module.exports = {
 
     return this.configPath;
   },
-
-  validateConfig(token, project, locales, format, root, outputPath, stripEmpty) {
+  validateConfig: function validateConfig(token, project, locales, format, root, outputPath, stripEmpty) {
     if (!token) {
       throw new Error('No `token` in ENV');
     } else {
@@ -63,70 +66,65 @@ module.exports = {
 
     return true;
   },
-
-  normalizeLocale(locale) {
+  normalizeLocale: function normalizeLocale(locale) {
     if (!locale) {
       return;
     }
 
-    const localeSegments = locale.replace('-', '_').split('_');
-    let newLocale = [];
-    newLocale.push(localeSegments[0].toLowerCase());
+    var localeSegments = locale.replace('-', '_').split('_');
+    var newLocale = [];
+    newLocale.push(localeSegments[0].toLowerCase()); // two segment locale
 
-    // two segment locale
     if (localeSegments[1]) {
       newLocale.push(localeSegments[1].toUpperCase());
     }
 
     return newLocale.join('_');
   },
-
-  downloadTranslation(locale) {
-    const normalizedLocale = this.normalizeLocale(locale);
+  downloadTranslation: function downloadTranslation(locale) {
+    var normalizedLocale = this.normalizeLocale(locale);
 
     if (!normalizedLocale) {
       throw new Error('No locale passed to download function');
-    }
+    } // if output folder doesn't exist we create it
 
-    // if output folder doesn't exist we create it
+
     if (!fs.existsSync(this.outputPath)) {
       fs.mkdirSync(this.outputPath);
     }
 
-    const fileName = `${normalizedLocale}.${this.format}`;
-    const filePath = path.join(process.cwd(), `${this.outputPath}/${fileName}`);
-    const url = `${this.apiHost}/api/v1/project/${this.project}/${locale}/${this.format}?apiToken=${this.token}&root=${this.root}&strip_empty=${this.stripEmpty}`;
+    var fileName = "".concat(normalizedLocale, ".").concat(this.format);
+    var filePath = path.join(process.cwd(), "".concat(this.outputPath, "/").concat(fileName));
+    var url = "".concat(this.apiHost, "/api/v1/project/").concat(this.project, "/").concat(locale, "/").concat(this.format, "?apiToken=").concat(this.token, "&root=").concat(this.root, "&strip_empty=").concat(this.stripEmpty);
+    var bufferFile; // if file exist we grab it's size
 
-    let bufferFile;
-
-    // if file exist we grab it's size
     if (fs.existsSync(filePath)) {
       bufferFile = fs.readFileSync(filePath);
-    }
+    } // we start stream
 
-    // we start stream
-    const file = fs.createWriteStream(filePath);
 
-    return new Promise((resolve, reject) => {
-      https.get(url, response => {
-        const { statusCode } = response;
+    var file = fs.createWriteStream(filePath);
+    return new Promise(function (resolve, reject) {
+      https.get(url, function (response) {
+        var statusCode = response.statusCode;
 
         if (statusCode !== 200) {
-          return reject(`Request Failed.\nStatus Code: ${statusCode}`);
+          return reject("Request Failed.\nStatus Code: ".concat(statusCode));
         }
 
         response.pipe(file);
-        file.on('finish', () => {
-          const newBufferFile = fs.readFileSync(filePath);
+        file.on('finish', function () {
+          var newBufferFile = fs.readFileSync(filePath);
 
           if (bufferFile && bufferFile.equals(newBufferFile)) {
-            log('yellow', `Generating "${locale}" translation. Skipped.`);
+            log('yellow', "Generating \"".concat(locale, "\" translation. Skipped."));
           } else {
-            log('green', `Generating "${locale}" translation. Done.`);
+            log('green', "Generating \"".concat(locale, "\" translation. Done."));
           }
+
           resolve();
         });
-      }).on('error', e => {
+      }).on('error', function (e) {
         log('red', e);
       });
     });
