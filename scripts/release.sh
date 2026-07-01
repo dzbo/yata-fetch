6e74bcdb499d2a045103af74119e0f68b345ae78
Dominik Zborowski <dominik.zborowski@gmail.com>
2026-07-01 13:37:06 +0200

    chore: Add configs + lint-staged

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 set -e
 
-echo "Release type?"
-select TYPE in patch minor major; do
-  case $TYPE in
-    patch|minor|major) break ;;
-  esac
-done
+TYPE=$1
+
+if [ -z "$TYPE" ]; then
+  echo "Release type?"
+  select TYPE in patch minor major; do
+    case $TYPE in
+      patch|minor|major) break ;;
+    esac
+  done
+fi
+
+case $TYPE in
+  patch|minor|major) ;;
+  *) echo "Invalid release type: $TYPE (expected patch, minor, or major)"; exit 1 ;;
+esac
 
 npm version $TYPE --no-git-tag-version
 NEW_VERSION=$(node -p "require('./package.json').version")
